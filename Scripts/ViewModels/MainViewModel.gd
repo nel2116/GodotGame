ec54eed719115255629f760747c60bf481c47a4d
nel2219 <Atameauki@outlook.jp>
2025-06-07 21:37:00 +0900

    [fix] Documentの整理

--- Scripts/ViewModels/MainViewModel.gd
+++
@@ -1,8 +0,0 @@
-class_name MainViewModel
-const ObservableProperty = preload("res://Scripts/Core/ObservableProperty.gd")
-extends Node
-
-@onready var message: ObservableProperty = ObservableProperty.new("")
-
-func set_message(text: String) -> void:
-    message.value = text
